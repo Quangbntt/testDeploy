269e568274154e3a5419c26042f859c19012f943
quangnd <quangnd.hn@havaz.vn>
2021-05-27 09:56:02 +0700

    fix

---
+++ build/precache-manifest.362eb1a82cf8e5e9d474c6f9a75a5501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f2c5aec32fc715ed92dcfbaa5cd71f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f1614d62c98c671ec32",
+    "url": "/static/css/main.84f78b1e.chunk.css"
+  },
+  {
+    "revision": "a4ab026cc80a1e9125cb",
+    "url": "/static/js/2.59c4dc7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.59c4dc7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1614d62c98c671ec32",
+    "url": "/static/js/main.5fbe878c.chunk.js"
+  },
+  {
+    "revision": "179934acaca5e548399c",
+    "url": "/static/js/runtime-main.e3c52469.js"
+  }
+]);
